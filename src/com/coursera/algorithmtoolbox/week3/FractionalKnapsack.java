4752d87b2f6aaa4855ba0c24c5cdfbd290cfe590
Swaran Singh Aulakh <swarnsinghaulakh@gmail.com>
2018-06-20 23:53:01 +0530

    Fractional Kanpsack Implemented.

--- src/com/coursera/algorithmtoolbox/week3/FractionalKnapsack.java
+++ src/com/coursera/algorithmtoolbox/week3/FractionalKnapsack.java
@@ -42,6 +42,10 @@ package com.coursera.algorithmtoolbox.week3;
 
 import com.coursera.util.FastScanner;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * @author Swarn Singh.
  */
@@ -52,5 +56,108 @@ public class FractionalKnapsack {
         int number = scanner.nextInt();
         int weight = scanner.nextInt();
 
+        Item[] items = new Item[number];
+
+        while (number > 0) {
+            number--;
+            Item item = new Item();
+            item.value = scanner.nextInt();
+            item.weight = scanner.nextInt();
+            item.profit = item.value / item.weight;
+            item.isLooted = false;
+            items[number] = item;
+        }
+
+        System.out.println("Maximum Profit : " + getMaxLootProfit(weight, items));
+    }
+
+    /**
+     *
+     * @param weight, here maximum knapsack weight
+     * @param items, no. of items available
+     * @return maximum profit
+     *
+     * Now each iteration is O(1)
+     *
+     * Knapsack after sorting is O(n)
+     *
+     * Sort + Knapsack is O(n log n)
+     *
+     */
+    private static double getMaxLootProfit(int weight, Item[] items) {
+        double totalProfit = 0;
+        Arrays.sort(items, Comparator.comparingDouble(Item::getProfit));
+        int n = items.length - 1;
+
+        while (weight > 0 && n >= 0) {
+            Item item = items[n];
+            n--;
+            if (item.weight <= weight) {
+                weight -= item.weight;
+                totalProfit += item.value;
+            } else {
+                if (item.weight > weight) {
+                    totalProfit += item.value / (item.weight / weight);
+                    weight = 0;
+                }
+            }
+            continue;
+        }
+        return totalProfit;
+    }
+
+    /**
+     *
+     * @param weight, here maximum knapsack weight
+     * @param items, no. of items available
+     * @return maximum profit
+     *
+     * It takes O(n^2) time
+     */
+    private static double getMaxProfit(int weight, Item[] items) {
+        double totalProfit = 0;
+        int n = items.length - 1;
+
+        while (weight > 0) {
+            double maxProfit = 0;
+            int pos = 0;
+            Item item = null;
+
+            for (int i = 0; i < items.length; i++) {
+                if (!items[i].isLooted && items[i].profit >= maxProfit) {
+                    item = items[i];
+                    maxProfit = item.profit;
+                    pos = i;
+                }
+            }
+
+            if (item != null) {
+                item.isLooted = true;
+                items[pos] = item;
+                if (item.weight <= weight) {
+                    weight -= item.weight;
+                    totalProfit += item.value;
+                } else {
+                    if (item.weight > weight) {
+                        totalProfit += item.value / (item.weight / weight);
+                        weight = 0;
+                    }
+                }
+            }
+            continue;
+        }
+
+        return totalProfit;
+    }
+
+    static class Item {
+        int value;
+        int weight;
+        boolean isLooted;
+        double profit;
+
+        public double getProfit() {
+            return profit;
+        }
     }
 }
